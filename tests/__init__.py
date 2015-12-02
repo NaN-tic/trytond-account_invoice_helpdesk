515a50d8cc8c1ed56b8192fd4335a9f6e0c95bd6
resteve <resteve@zikzakmedia.com>
2015-12-02 09:49:25 +0100

    FIX loading Tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
-# This file is part of the invoice_helpdesk module for Tryton.
+# This file is part of the account_invoice_helpdesk module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from .test_invoice_helpdesk import suite
+from .test_account_invoice_helpdesk import suite
 
 __all__ = ['suite']
