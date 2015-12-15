2ddf50e2ed400f1dc523d3ccc92ada685818868a
resteve <resteve@zikzakmedia.com>
2015-12-15 17:10:22 +0100

    Getmail kind

--- helpdesk.py
+++ helpdesk.py
@@ -1,4 +1,4 @@
-# This file is part of the invoice_helpdesk module for Tryton.
+# This file is part of the account_invoice_helpdesk module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.model import ModelSQL, fields
